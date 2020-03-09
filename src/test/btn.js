e1e2b255edc68a0c1ed22d8cc8c133dcfcbe4231
undind <flyker777@yandex.ru>
2020-03-09 17:06:24 +0300

    Some fixed

--- src/test/btn.js
+++ src/test/btn.js
@@ -1,7 +1,7 @@
 it("На странице есть кнопка Add ticket", async () => {
   await browser.url("http://localhost:3000/tickets");
-  const btn = await $('#basic-button');
-  const existing = await btn.isExisting();
-  console.log(browser)
-  return existing;
+  const elem = await browser.$("#basic-button");
+  await elem.click();
+  console.log(elem.myFirefoxBrowser)
+  await elem.myFirefoxBrowser.isExisting();
 });
